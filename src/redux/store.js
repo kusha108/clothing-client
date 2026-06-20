e30ea2ff55edf9c56052f9b16b0a44d7e16798aa
kusha108 <2k22.csai.2213254@gmail.com>
2026-06-21 02:07:52 +0530

    final frontend deployment fixes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 
-import { composeWithDevTools } from 'redux-devtools-extension';
-
 import { cartReducer } from './reducers/cartReducer';
 import { getProductDetailsReducer, getProductReducer } from './reducers/productReducer';
 
@@ -10,14 +8,13 @@ const reducer = combineReducers({
     cart: cartReducer,
     getProducts: getProductReducer,
     getProductDetails: getProductDetailsReducer
-})
-
+});
 
 const middleware = [thunk];
 
 const store = createStore(
-    reducer, 
-    composeWithDevTools(applyMiddleware(...middleware))
+    reducer,
+    applyMiddleware(...middleware)
 );
 
 export default store;
--- src/service/aiService.js
+++ src/service/aiService.js
@@ -1,24 +1,46 @@
 
+// import axios from "axios";
+
+// export const analyzeImage = async (file, gender) => {
+//   const formData = new FormData();
+
+//   //  FIX 1: backend expects "file"
+//   formData.append("file", file);
+
+//   //  FIX 2: send gender
+//   formData.append("gender", gender);
+
+//   const res = await axios.post(
+//     "https://clothing-server-tpsh.onrender.com/api/ai/analyze",
+//     formData,
+//     {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//     }
+//   );
+
+//   return res.data;
+// };
+
 import axios from "axios";
 
 export const analyzeImage = async (file, gender) => {
   const formData = new FormData();
 
-  //  FIX 1: backend expects "file"
   formData.append("file", file);
-
-  //  FIX 2: send gender
   formData.append("gender", gender);
 
-  const res = await axios.post(
-    "https://clothing-server-tpsh.onrender.com/api/ai/analyze",
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+  const API_URL =
+    process.env.NODE_ENV === "production"
+      ? "https://clothing-server-tpsh.onrender.com/api/ai/analyze"
+      : "http://localhost:8000/api/ai/analyze";
+
+  const res = await axios.post(API_URL, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
   return res.data;
 };
