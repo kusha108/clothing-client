c3a01a6af3a784410dbd59ef76c3d1bd7be2c647
kusha108 <2k22.csai.2213254@gmail.com>
2026-06-21 00:54:36 +0530

    update production backend urls

--- src/service/aiService.js
+++ src/service/aiService.js
@@ -1,37 +1,17 @@
-// import axios from "axios";
-
-// export const analyzeImage = async (file) => {
-//   const formData = new FormData();
-//   formData.append("image", file);
-
-//   const res = await axios.post(
-//     "http://localhost:8000/api/ai/analyze",
-//     formData,
-//     {
-//       headers: {
-//         "Content-Type": "multipart/form-data",
-//       },
-//     }
-//   );
-
-//   return res.data;
-// };
-
-
 
 import axios from "axios";
 
 export const analyzeImage = async (file, gender) => {
   const formData = new FormData();
 
-  // 🔥 FIX 1: backend expects "file"
+  //  FIX 1: backend expects "file"
   formData.append("file", file);
 
-  // 🔥 FIX 2: send gender
+  //  FIX 2: send gender
   formData.append("gender", gender);
 
   const res = await axios.post(
-    "http://localhost:8000/api/ai/analyze",
+    "https://clothing-server-tpsh.onrender.com/api/ai/analyze",
     formData,
     {
       headers: {
--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://clothing-server-tpsh.onrender.com';
 export const authenticateSignup = async (data) => {
       try {
          return  await axios.post(`${URL}/signup`, data);
